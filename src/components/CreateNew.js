db8f88e02894a7cd91599017fbe9207645c50e58
jim4067 <jimmyimpulse2@gmail.com>
2020-10-06 00:22:04 +0300

    feat: separated the code into components

---
+++ src/components/CreateNew.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useField } from '../hooks/index';
+
+const CreateNew = ({ newAnecdote }) => {
+    /*
+    const [content, setContent] = useState('')
+    const [author, setAuthor] = useState('')
+    const [info, setInfo] = useState('')
+    */
+    const content = useField("text");
+    const author = useField("text");
+    const info = useField("text");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const id = (Math.random() * 10000).toFixed(0);
+
+        const newAnecdote = ({
+            id : id,
+            content: content.value,
+            author: author.value,
+            info: info.value,
+            votes: 0
+        })
+    };
+
+    return (
+        <div>
+            <h2>create a new anecdote</h2>
+
+            <form onSubmit={handleSubmit}>
+                content
+				<input {...content} />
+                <br />
+				author
+				<input {...author} />
+                <br />
+				url for more info
+				<input {...info} />
+                <br />
+                <button type="submit">create</button>
+            </form>
+
+        </div>
+    );
+};
+
+export default CreateNew;
